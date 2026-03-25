a6c8f2e06534d690e374450473e877f3be670552
amrithadevi06 <amrithaashok06@gmail.com>
2026-03-25 14:50:54 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,10 +5,13 @@ public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
+        // Welcome Message
         System.out.println("=== Train Consist Management App ===");
 
+        // Initialize Train Consist (Empty List)
         List<String> bogies = new ArrayList<>();
 
+        // Display Initial Bogie Count
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + bogies.size());
     }
